cd28f9f6d8b865c27dd5e9c8a8457e2f265b2be3
Crspy <crspy8687@gmail.com>
2024-05-03 15:51:34 -0700

    deleted favicon file made by vercel
    I fucking hate myself

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
+    <body className={inter.className}>
+    {children}
       </body>
     </html>
   );
