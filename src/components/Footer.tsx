acbea6308c8f2b9d2bd9632aa94a8d1b0c8ccdee
Crspy <crspy8687@gmail.com>
2024-05-03 15:43:53 -0700

    Added footer component
    Added a simple component section to the main page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import {Copyright} from "lucide-react";
+
+const Footer = () => {
+    return (
+        <footer className="mb-12 text-gray-200 justify-center items-center flex gap-3 font-poppins">
+            <Copyright />
+            <p>Copyright 2024 Crspy — All Rights Reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
